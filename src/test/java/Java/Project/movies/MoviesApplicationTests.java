73cea341c616644e990870a186a5b5965af2a4d5
Zaki <reborn7875@gmail.com>
2024-02-03 18:06:39 +0800

    docs:first commit

---
+++ src/test/java/Java/Project/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package Java.Project.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
