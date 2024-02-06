85de1242ab4ea483cd40152dae99b63d2957c58f
Zaki <reborn7875@gmail.com>
2024-02-06 09:35:21 +0800

    feat: Get all movie API and get per imdbId, post new review API

--- src/main/java/Java/Project/movies/MoviesApplication.java
+++ src/main/java/Java/Project/movies/MoviesApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class MoviesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoviesApplication.class, args);
-	}
+
+  public static void main(String[] args) {
+    SpringApplication.run(MoviesApplication.class, args);
+  }
+
 
 }
